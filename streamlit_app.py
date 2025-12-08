83a9d9b23966ed28f89a0482e7fa1ab752176948
Esther <89661502+estherfba@users.noreply.github.com>
2025-12-07 22:16:43 -0300

    vai

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,10 @@ import pandas as pd
 df = pd.read_csv("https://raw.githubusercontent.com/estherfba/Pojetinho/refs/heads/main/Biblioteca.csv")
 
 st.title("Match literário da Esther")
-st.write("Olá, queridos! Você já pensou 'nossa, queria tanto ler um livro de __________' mas não sabe nem por onde começar a procurar? Isso é pra você!")
+st.write("Olá, querido (a)! Você já pensou 'nossa, queria tanto ler um livro de __________' mas não sabe nem por onde começar a procurar? Isso é pra você!")
 
 st.write("Nada mais que um projetinho de férias de verão, fruto da abstinência (voluntária, to bem) de crusader kings/the sims/netflix, espero que seja útil a alguém. Ainda ta na versão betinha kkkk fique a vontade para dar o seu feedback!")
-st.write("Responda às perguntas abaixo e receba uma recomendação literária personalizada! Obviamente a base de dados se restringe ao meu acervo de livros... daqui uns anos será maior, se Deus quiser.")
+st.write("Sem mais delongas, responda às perguntas abaixo e receba uma recomendação literária personalizada! Obviamente a base de dados se restringe ao meu acervo de livros... daqui uns anos será maior, se Deus quiser.")
 st.write("OBS: tenha em vista que, assim como no amor, se colocar muito critério fica difícil encontrar um match. Todavia, obviamente, caso ache é mais certeiro.")
 
 idioma = st.radio(
